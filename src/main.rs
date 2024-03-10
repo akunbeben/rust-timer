5d0139b60d46e39078dd3215445cc46c86ec7b96
Benny Rahmat <akunbeben@gmail.com>
2024-03-11 03:33:36 +0800

    add missing space

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     while tm.start_time.is_some() {
         let position = Mouse::get_mouse_position();
         match position {
-            Mouse::Position { x, y } => print!("\rx: {}, y: {}", x, y),
+            Mouse::Position { x, y } => print!("\rx: {}, y: {} ", x, y),
             Mouse::Error => print!("\rError getting mouse position"),
        }
     }
