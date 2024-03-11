ce85f90cddb78e184c5462cdff17af1a138f90c9
Benny Rahmat <akunbeben@gmail.com>
2024-03-12 03:10:39 +0800

    try connect to sqlite using rusqlite

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,50 @@
-// use crate::timer::Timer;
-use mouse_position::mouse_position::Mouse;
+use user_idle::UserIdle;
+use rusqlite::{params, Connection, Result};
+use chrono::Local;
 
-// mod timer;
+#[derive(Debug)]
+struct Data {
+    id: i32,
+    date: String,
+    duration: u64,
+}
 
-fn main() {
-    let turn_on = true;
-    // let tm = Timer::start_timer();
+fn main() -> Result<()> {
+    let daemonized = true;
+    let threshold = 5;
+    let mut idle_duration: u64 = 0;
+    let conn = Connection::open_in_memory()?;
 
-    while turn_on {
-        let position = Mouse::get_mouse_position();
-        match position {
-            Mouse::Position { x, y } => print!("\rx: {}, y: {} ", x, y),
-            Mouse::Error => print!("\rError getting mouse position"),
-       }
+    while daemonized {
+        let idle = UserIdle::get_time().unwrap();
+
+        conn.execute(
+            "CREATE TABLE IF NOT EXISTS data (id INTEGER PRIMARY KEY, date DATE NOT NULL, duration BIGINT NOT NULL)",
+            [],
+        )?;
+
+        if idle.as_seconds() > threshold {
+            idle_duration = idle.as_seconds() - threshold;
+
+            let prepared = Data {
+                id: 0,
+                date: Local::now().format("%Y-%m-%d").to_string(),
+                duration: idle_duration,
+            };
+
+            let _ = conn.execute("
+                INSERT OR REPLACE INTO data (id, date, duration) values (
+                    (SELECT id FROM data WHERE date = ?1),
+                    (SELECT date FROM data WHERE date = ?1),
+                    ?2
+                )
+            ", params![prepared.date, prepared.duration]);
+
+            print!("\r{}", idle_duration);
+        } else {
+            print!("\r{}", idle_duration);
+        }
     }
+
+    Ok(())
 }
